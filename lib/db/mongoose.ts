b7ff9206b836ae95b663c5b4424a8f930568e27b
gflores_crp <gflores@crp.pe>
2024-04-22 18:14:45 -0500

    FIXED: Conexion a mongoDB

--- lib/db/mongoose.ts
+++ lib/db/mongoose.ts
@@ -21,4 +21,8 @@ export const connectToDatabase = async () => {
     if (!MONGODB_URL) throw new Error('MONGODB_URL is not defined');
 
     cached.promise = cached.promise || mongoose.connect(MONGODB_URL, { dbName: 'imaginify', bufferCommands: false })
+    
+    cached.conn = await cached.promise;
+
+    return cached.conn;
 }
